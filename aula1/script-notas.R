2e5d2e05e325988e77d685d752791adb8e4a7cee
Edson Fagner da Silva Cristovam <pg2006pe@hotmail.com>
2021-11-03 21:18:07 -0300

    update script notas

--- aula1/script-notas.R
+++ aula1/script-notas.R
@@ -1,4 +1,14 @@
 notas <- read.table("notas.txt", head=T)
-attach(notas) #ctrl+R para executar#
+attach(notas) # ctrl+R para executar
 Notas
 shapiro.test(Notas)
+x <- rnorm(50)
+x
+ks.test(x, Notas)
+ks.test(Notas, x)
+
+
+#x <- rnorm(length(Notas))
+#x
+#ks.test(x, Notas)
+#ks.test(Notas, x)
